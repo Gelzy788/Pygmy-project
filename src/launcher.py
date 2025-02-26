a68e7cd380584b2cad5b188ac21ee5d0773f51df
Gelzy788 <negm.ali@mail.ru>
2025-02-27 01:00:03 +0300

    db fix

--- src/launcher.py
+++ src/launcher.py
@@ -9,7 +9,7 @@ from database import Database
 # Инициализация Pygame
 pygame.init()
 
-# Настройки экран
+# Настройки экрана
 WIDTH, HEIGHT = 800, 600
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Vampire Game Launcher")
